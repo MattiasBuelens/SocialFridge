cd0a5a3c5afa9bb7383f92cbabdc2a16334924d9
Mattias Buelens <mattias@buelens.com>
2014-03-14 15:50:34 +0100

    Added Facebook login sample.
    Now to somehow authenticate at App Engine with the Session's access
    token...

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/LoginActivity.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/LoginActivity.java
@@ -1,11 +1,17 @@
 package be.kuleuven.cs.chikwadraat.socialfridge;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-import be.kuleuven.cs.chikwadraat.socialfridge.app.R;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
 
 public class LoginActivity extends Activity {
 
@@ -13,12 +19,37 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // start Facebook Login
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+                    // make request to the /me API
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            if (user != null) {
+                                TextView hello = (TextView) findViewById(R.id.hello_world);
+                                hello.setText("Hello " + user.getName() + "!");
+                            }
+                        }
+                    }).executeAsync();
+                }
+            }
+        });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.login, menu);
         return true;
