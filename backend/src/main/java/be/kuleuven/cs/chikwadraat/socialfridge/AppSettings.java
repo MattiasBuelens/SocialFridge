44978bfa6b4a74fd5f10c66695b83d1e852cc0f6
Mattias Buelens <mattias@buelens.com>
2014-03-15 15:34:07 +0100

    Working on Facebook authentication.

---
+++ backend/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/AppSettings.java
@@ -0,0 +1,32 @@
+package be.kuleuven.cs.chikwadraat.socialfridge;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Application settings.
+ * Read from /resources/app.properties
+ */
+public final class AppSettings {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(AppSettings.class.getClassLoader().getResourceAsStream("/app.properties"));
+        } catch (IOException e) {
+        }
+    }
+
+    private AppSettings() {
+    }
+
+    public static String getFacebookAppId() {
+        return properties.getProperty("fb_app_id");
+    }
+
+    public static String getFacebookAppSecret() {
+        return properties.getProperty("fb_app_secret");
+    }
+
+}
