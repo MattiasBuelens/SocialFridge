0e05bfc1bb2d9a69d2b4e83e18cc2360b11fcde2
Mattias Buelens <mattias@buelens.com>
2014-03-18 15:35:03 +0100

    Set permissions on login button.

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/LoginFragment.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/LoginFragment.java
@@ -6,15 +6,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.widget.LoginButton;
+
 /**
  * Login fragment.
  */
 public class LoginFragment extends Fragment {
 
+    private LoginButton loginButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.login, container, false);
+
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+
+        init(savedInstanceState);
+
         return view;
+    }
+
+    private void init(Bundle savedInstanceState) {
+        // Request public profile and friends list
+        loginButton.setReadPermissions("basic_info");
     }
 
 }
