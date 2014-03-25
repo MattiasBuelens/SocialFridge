b8517f6b2052e69b1a133344e5d1e96b343dfec6
Mattias Buelens <mattias@buelens.com>
2014-03-25 18:48:38 +0100

    Working on candidates list.

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/PartyActivity.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/PartyActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.Session;
@@ -49,6 +51,10 @@ public class PartyActivity extends BaseActivity {
         List<User> users = ImmutableList.of(user1, user2, user3);
 
         partnersList.setAdapter(new PartnersListAdapter(this, users));
+
+        ListView candidatesList = (ListView) findViewById(R.id.party_candidates_list);
+
+        candidatesList.setAdapter(new CandidatesListAdapter(this, users));
     }
 
     public static class PartnersListAdapter extends ArrayAdapter<User> {
@@ -58,7 +64,7 @@ public class PartyActivity extends BaseActivity {
         }
 
         public PartnersListAdapter(Context context, List<User> partners) {
-            super(context, R.layout.dish_list_item, partners);
+            super(context, R.layout.partner_list_item, partners);
         }
 
         @Override
@@ -72,6 +78,47 @@ public class PartyActivity extends BaseActivity {
 
             User user = getItem(position);
             pictureView.setProfileId(user.getId());
+
+            return view;
+        }
+    }
+
+    public static class CandidatesListAdapter extends ArrayAdapter<User> {
+
+        public CandidatesListAdapter(Context context, User[] candidates) {
+            this(context, Arrays.asList(candidates));
+        }
+
+        public CandidatesListAdapter(Context context, List<User> candidates) {
+            super(context, R.layout.candidate_list_item, candidates);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = View.inflate(getContext(), R.layout.candidate_list_item, null);
+            }
+
+            ProfilePictureView pictureView = (ProfilePictureView) view.findViewById(R.id.candidate_pic);
+            TextView nameView = (TextView) view.findViewById(R.id.candidate_name);
+            Button inviteButton = (Button) view.findViewById(R.id.candidate_invite);
+            Button cancelInviteButton = (Button) view.findViewById(R.id.candidate_cancel_invite);
+
+            User user = getItem(position);
+            pictureView.setProfileId(user.getId());
+            nameView.setText(user.getName());
+
+            // TODO Dummy condition
+            if((user.hashCode() & 1) == 1) {
+                inviteButton.setText(R.string.party_partner_status_invited);
+                inviteButton.setEnabled(false);
+                cancelInviteButton.setVisibility(View.VISIBLE);
+            } else {
+                inviteButton.setText(R.string.party_action_invite);
+                inviteButton.setEnabled(true);
+                cancelInviteButton.setVisibility(View.INVISIBLE);
+            }
 
             return view;
         }
