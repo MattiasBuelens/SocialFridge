739d15c081f79d4b574abff6d3da10792edfd83c
MilanSamyn <milansamyn@gmail.com>
2014-03-31 14:14:44 +0200

    Time Slots
    strings toegevoegd voor titeltjes
    choose final slot xml aangemaakt
    klein beetje activity

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/TimeSlotActivity.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/TimeSlotActivity.java
@@ -1,15 +1,29 @@
 package be.kuleuven.cs.chikwadraat.socialfridge;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 /**
  * Time slot activity.
  * Choose time slots.
  */
-public class TimeSlotActivity extends BaseActivity {
+public class TimeSlotActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_timeslots);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.time_action_find_partners:
+
+
+        }
     }
 
 }
