e8827df1b2249ce028c62f937091f0daa57f1665
Mattias Buelens <mattias@buelens.com>
2014-04-08 19:48:54 +0200

    Working on TimeSlotPickerFragment.

---
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/party/TimeSlotPickerFragment.java
@@ -0,0 +1,175 @@
+package be.kuleuven.cs.chikwadraat.socialfridge.party;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.GridView;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.kuleuven.cs.chikwadraat.socialfridge.ArrayAdapter;
+import be.kuleuven.cs.chikwadraat.socialfridge.R;
+
+/**
+ * A fragment to pick one time slot.
+ */
+public class TimeSlotPickerFragment extends Fragment {
+
+    private static final String ARG_PICKED_TIME_SLOT = "picked_time_slot";
+    private static final String ARG_TIME_SLOTS = "time_slots";
+
+    private TimeSlotSelection pickedTimeSlot;
+    private ArrayList<TimeSlotSelection> timeSlots;
+
+    private GridView timeSlotGrid;
+    private TimeSlotPickerArrayAdapter timeSlotAdapter;
+
+    /**
+     * Create a new time slot picker fragment.
+     *
+     * @param timeSlots The time slots to choose from.
+     * @return A new instance of fragment TimeSlotPickerFragment.
+     */
+    public static TimeSlotPickerFragment newInstance(List<TimeSlotSelection> timeSlots) {
+        TimeSlotPickerFragment fragment = new TimeSlotPickerFragment();
+        fragment.setTimeSlots(timeSlots);
+        return fragment;
+    }
+
+    public TimeSlotPickerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_timeslots, container, false);
+
+        timeSlotGrid = (GridView) view.findViewById(R.id.time_slot_grid);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            // Restore
+            pickedTimeSlot = savedInstanceState.getParcelable(ARG_PICKED_TIME_SLOT);
+            timeSlots = savedInstanceState.getParcelableArrayList(ARG_TIME_SLOTS);
+        }
+
+        timeSlotAdapter = new TimeSlotPickerArrayAdapter(getActivity(), timeSlots);
+        timeSlotAdapter.setCheckedItem(pickedTimeSlot);
+        timeSlotGrid.setAdapter(timeSlotAdapter);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelable(ARG_PICKED_TIME_SLOT, getPickedTimeSlot());
+        outState.putParcelableArrayList(ARG_TIME_SLOTS, timeSlots);
+    }
+
+    public TimeSlotSelection getPickedTimeSlot() {
+        if (timeSlotAdapter != null) {
+            return timeSlotAdapter.getCheckedItem();
+        } else {
+            return pickedTimeSlot;
+        }
+    }
+
+    public List<TimeSlotSelection> getTimeSlots() {
+        return timeSlots;
+    }
+
+    public void setTimeSlots(List<TimeSlotSelection> timeSlots) {
+        this.timeSlots = new ArrayList<TimeSlotSelection>(timeSlots);
+        if (timeSlotAdapter != null) {
+            timeSlotAdapter.setData(this.timeSlots);
+        }
+    }
+
+    public static class TimeSlotPickerArrayAdapter extends ArrayAdapter<TimeSlotSelection> implements CompoundButton.OnCheckedChangeListener {
+
+        private int checkedPosition = -1;
+
+        public TimeSlotPickerArrayAdapter(Context context, List<TimeSlotSelection> selections) {
+            super(context, R.layout.timeslot, selections);
+        }
+
+        public int getCheckedPosition() {
+            return checkedPosition;
+        }
+
+        public void setCheckedPosition(int checkedPosition) {
+            this.checkedPosition = checkedPosition;
+            notifyDataSetChanged();
+        }
+
+        public TimeSlotSelection getCheckedItem() {
+            int position = getCheckedPosition();
+            if (position >= 0 && position < getCount()) {
+                return getItem(position);
+            } else {
+                return null;
+            }
+        }
+
+        public void setCheckedItem(TimeSlotSelection item) {
+            setCheckedPosition(getPosition(item));
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            ViewHolder vh;
+            if (view == null) {
+                view = View.inflate(getContext(), R.layout.timeslot_pick, null);
+                vh = new ViewHolder();
+                vh.radioButton = (RadioButton) view.findViewById(R.id.time_slot_pick);
+                view.setTag(vh);
+            } else {
+                vh = (ViewHolder) view.getTag();
+            }
+
+            TimeSlotSelection slot = getItem(position);
+            boolean isChecked = (getCheckedPosition() == position);
+            vh.position = position;
+            String text = slot.getBeginHour() + "h - " + slot.getEndHour() + "h";
+            vh.radioButton.setText(text);
+            vh.radioButton.setChecked(isChecked);
+            vh.radioButton.setEnabled(slot.isIncluded());
+            vh.radioButton.setOnCheckedChangeListener(this);
+            vh.radioButton.setTag(vh);
+
+            return view;
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            ViewHolder vh = (ViewHolder) buttonView.getTag();
+            TimeSlotSelection slot = getItem(vh.position);
+            if (!slot.isIncluded()) return;
+            if (isChecked) {
+                setCheckedItem(slot);
+            }
+        }
+
+        private static class ViewHolder {
+            RadioButton radioButton;
+            int position;
+        }
+
+    }
+
+}
