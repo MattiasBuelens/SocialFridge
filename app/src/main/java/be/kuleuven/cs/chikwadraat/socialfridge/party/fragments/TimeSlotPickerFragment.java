f1f730829f032d2938349f9d5b32b98d8866eee9
Mattias Buelens <mattias@buelens.com>
2014-04-09 16:00:47 +0200

    Fixed using wrong layout for time slot picker.

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/party/fragments/TimeSlotPickerFragment.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/party/fragments/TimeSlotPickerFragment.java
@@ -51,7 +51,7 @@ public class TimeSlotPickerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_timeslots, container, false);
+        View view = inflater.inflate(R.layout.fragment_pick_timeslot, container, false);
 
         timeSlotGrid = (GridView) view.findViewById(R.id.time_slot_grid);
 
