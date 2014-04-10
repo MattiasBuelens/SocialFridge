daff39562bd24c365eb6764611124b093e76ef37
Mattias Buelens <mattias@buelens.com>
2014-04-10 14:43:50 +0200

    Removed listener on PlaceFragment.

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/PlaceFragment.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/PlaceFragment.java
@@ -1,7 +1,5 @@
 package be.kuleuven.cs.chikwadraat.socialfridge;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,15 +7,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PlaceFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link PlaceFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
 public class PlaceFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
@@ -28,8 +21,6 @@ public class PlaceFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     /**
      * Use this factory method to create a new instance of
@@ -48,6 +39,7 @@ public class PlaceFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     public PlaceFragment() {
         // Required empty public constructor
     }
@@ -66,45 +58,6 @@ public class PlaceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_place, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 }
