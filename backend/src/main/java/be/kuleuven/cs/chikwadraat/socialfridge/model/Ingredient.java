b961c38157f359e2244301947a0349ff4873083a
MilanSamyn <milansamyn@gmail.com>
2014-04-23 17:04:06 +0200

    Ingredient en Units in backend

---
+++ backend/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/model/Ingredient.java
@@ -0,0 +1,166 @@
+package be.kuleuven.cs.chikwadraat.socialfridge.model;
+
+import com.google.api.server.spi.config.AnnotationBoolean;
+import com.google.api.server.spi.config.ApiResourceProperty;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.images.ServingUrlOptions;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Ref;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import be.kuleuven.cs.chikwadraat.socialfridge.ImagesService;
+
+/**
+ * Created by Milan Samyn on 23/04/2014.
+ */
+@Entity(name = Ingredient.KIND)
+public class Ingredient {
+
+    public static final int THUMBNAIL_SIZE = 256;
+
+    public static final String KIND = "Ingredient";
+
+    @Id
+    private Long id;
+
+    /*
+     * Ingredient name.
+     */
+    @Index
+    private String name;
+
+    /**
+     * Ingredient category.
+     */
+    @Index
+    private Category category;
+
+    /**
+     * Ingredient unit of measurement
+     */
+    private UnitOfMeasurement.Unit unit;
+
+    /**
+     * Blob key of ingredient picture.
+     */
+    private BlobKey pictureKey;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(long id) {
+        this.id = id;
+    }
+
+    public static Key<Ingredient> getKey(long ingredientID) {
+        return Key.create(Ingredient.class, ingredientID);
+    }
+
+    public static Ref<Ingredient> getRef(long ingredientID) {
+        return Ref.create(getKey(ingredientID));
+    }
+
+    /**
+     * User ID.
+     */
+    public Long getID() {
+        return id;
+    }
+
+    /**
+     * Ingredient name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Ingredient category.
+     */
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    /**
+     * Ingredient unit of measurement
+     */
+    public UnitOfMeasurement.Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnitOfMeasurement(UnitOfMeasurement.Unit unit) {
+        this.unit = unit;
+    }
+
+    /**
+     * Blob key of ingredient picture.
+     */
+    @ApiResourceProperty(ignored = AnnotationBoolean.TRUE)
+    public BlobKey getPictureKey() {
+        return pictureKey;
+    }
+
+    public void setPictureKey(BlobKey pictureKey) {
+        this.pictureKey = pictureKey;
+    }
+
+    /**
+     * URL for ingredient picture.
+     */
+    public String getPictureURL() {
+        ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(getPictureKey());
+        return ImagesService.get().getServingUrl(options);
+    }
+
+    public String getPictureURL(int imageSize) {
+        ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(getPictureKey())
+                .imageSize(imageSize);
+        return ImagesService.get().getServingUrl(options);
+    }
+
+    public String getPictureURL(int imageSize, boolean crop) {
+        ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(getPictureKey())
+                .imageSize(imageSize)
+                .crop(crop);
+        return ImagesService.get().getServingUrl(options);
+    }
+
+    /**
+     * URL for ingredient thumbnail.
+     */
+    public String getThumbnailURL() {
+        return getPictureURL(THUMBNAIL_SIZE, true);
+    }
+
+
+    public static enum Category {
+
+        FATS,
+
+        DAIRY,
+
+        MEAT,
+
+        VEGETABLES,
+
+        CEREALS,
+
+        FISH,
+
+        FRUIT;
+
+    }
+
+
+
+}
---
+++ backend/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/model/UnitOfMeasurement.java
@@ -0,0 +1,16 @@
+package be.kuleuven.cs.chikwadraat.socialfridge.model;
+
+/**
+ * Created by Milan Samyn on 23/04/2014.
+ */
+public class UnitOfMeasurement {
+
+    public static enum Unit {
+
+        QUANTITY,
+
+        LIT
+
+
+    }
+}
