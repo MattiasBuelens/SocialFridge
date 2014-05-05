7da8588a154fdbab812bff4fcff3badea763523e
Mattias Buelens <mattias@buelens.com>
2014-05-05 12:37:29 +0200

    Decoupled IngredientsFragment.

--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/fridge/FridgeActivity.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/fridge/FridgeActivity.java
@@ -1,42 +1,24 @@
 package be.kuleuven.cs.chikwadraat.socialfridge.fridge;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.ListView;
 
-import com.facebook.Session;
-import com.google.android.gms.analytics.HitBuilders;
+import java.util.ArrayList;
+import java.util.List;
 
-import be.kuleuven.cs.chikwadraat.socialfridge.Endpoints;
-import be.kuleuven.cs.chikwadraat.socialfridge.ListActivity;
+import be.kuleuven.cs.chikwadraat.socialfridge.BaseActivity;
 import be.kuleuven.cs.chikwadraat.socialfridge.R;
-import be.kuleuven.cs.chikwadraat.socialfridge.endpoint.Endpoint;
+import be.kuleuven.cs.chikwadraat.socialfridge.measuring.Unit;
 import be.kuleuven.cs.chikwadraat.socialfridge.model.FridgeItem;
 import be.kuleuven.cs.chikwadraat.socialfridge.model.Measure;
-import be.kuleuven.cs.chikwadraat.socialfridge.model.Party;
-import be.kuleuven.cs.chikwadraat.socialfridge.party.BasePartyActivity;
-import be.kuleuven.cs.chikwadraat.socialfridge.party.PlanPartyActivity;
 import be.kuleuven.cs.chikwadraat.socialfridge.util.ObservableAsyncTask;
 
 /**
  * Manage fridge activity.
  */
-public class FridgeActivity extends ListActivity implements SearchView.OnQueryTextListener, SearchView.OnCloseListener,
-        MeasureDialog.OnMeasureSetListener, ObservableAsyncTask.Listener<Void, Void>, View.OnClickListener {
+public class FridgeActivity extends BaseActivity implements ObservableAsyncTask.Listener<Void, Void>, View.OnClickListener, IngredientsFragment.IngredientsListener {
 
     private static final String TAG = "FridgeActivity";
 
@@ -44,14 +26,6 @@ public class FridgeActivity extends ListActivity implements SearchView.OnQueryTe
     private Button addIngredientsButton;
 
     private AddIngredientsTask task;
-
-    private static final String STATE_QUERY = "fridge_search_query";
-    private static final String STATE_EDITING_ITEM = "fridge_editing_item";
-
-    private int editingItemPosition;
-
-    private SearchView searchView;
-    private String searchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,73 +39,10 @@ public class FridgeActivity extends ListActivity implements SearchView.OnQueryTe
 
         //TODO Re-attach to close invites task ?
 
-        if (savedInstanceState != null) {
-            searchQuery = savedInstanceState.getString(STATE_QUERY);
-            editingItemPosition = savedInstanceState.getInt(STATE_EDITING_ITEM, -1);
-        }
-
-        handleIntent(getIntent());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.fridge_search, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.search);
-        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        searchView.setQueryHint(getString(R.string.fridge_search_hint));
-        searchView.setOnQueryTextListener(this);
-        searchView.setOnCloseListener(this);
-
-        if (!TextUtils.isEmpty(searchQuery)) {
-            String query = searchQuery;
-            MenuItemCompat.expandActionView(searchItem);
-            searchIngredients(query);
-        }
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
-        handleIntent(intent);
-    }
-
-    protected void handleIntent(Intent intent) {
-        if (intent == null) return;
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            searchIngredients(query);
-        }
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        searchIngredients(newText);
-        return true;
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        // When the search is "committed" by the user, then hide the keyboard so the user can
-        // more easily browse the list of results.
-        if (searchView != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            if (imm != null) {
-                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
-            }
-            searchView.clearFocus();
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onClose() {
-        searchIngredients(null);
-        return false;
+        // TODO Remove dummy items
+        List<FridgeItem> items = new ArrayList<FridgeItem>();
+        items.add(new FridgeItem("Eggs", R.drawable.eggs, new Measure(6, Unit.PIECES)));
+        ingredientsFragment.setIngredients(items);
     }
 
     @Override
@@ -154,14 +65,6 @@ public class FridgeActivity extends ListActivity implements SearchView.OnQueryTe
         // do nothing
     }
 
-    protected void searchIngredients(String query) {
-        searchQuery = query;
-        if (searchView != null && !TextUtils.equals(searchView.getQuery(), query)) {
-            searchView.setQuery(searchQuery, false);
-        }
-        // TODO HOWTO itemsArrayAdapter.getFilter().filter(query);
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -180,38 +83,13 @@ public class FridgeActivity extends ListActivity implements SearchView.OnQueryTe
     }
 
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        FridgeItem item = (FridgeItem) l.getItemAtPosition(position);
-        editingItemPosition = position;
-
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        Fragment current = getSupportFragmentManager().findFragmentByTag("dialog");
-        MeasureDialogFragment fragment;
-        if (current != null) {
-            fragment = (MeasureDialogFragment) current;
-        } else {
-            fragment = MeasureDialogFragment.newInstance();
-        }
-        fragment.setTitle(item.getName());
-        fragment.setMeasure(item.getQuantity());
-        fragment.show(ft, "dialog");
+    public void onFridgeItemUpdated(FridgeItem item) {
+        // TODO
     }
 
     @Override
-    public void onMeasureSet(Measure measure) {
-        if (editingItemPosition >= 0) {
-            FridgeItem item = (FridgeItem) getListView().getItemAtPosition(editingItemPosition);
-            item.setQuantity(measure);
-            //TODO itemsArrayAdapter.notifyDataSetChanged();
-        }
-        editingItemPosition = -1;
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(STATE_QUERY, searchView.getQuery().toString());
-        outState.putInt(STATE_EDITING_ITEM, editingItemPosition);
+    public void onFridgeItemRemoved(FridgeItem item) {
+        // TODO
     }
 
     private static class AddIngredientsTask extends ObservableAsyncTask<Void, Void, Void> {
--- app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/fridge/IngredientsFragment.java
+++ app/src/main/java/be/kuleuven/cs/chikwadraat/socialfridge/fridge/IngredientsFragment.java
@@ -5,9 +5,18 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -17,24 +26,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import be.kuleuven.cs.chikwadraat.socialfridge.R;
-import be.kuleuven.cs.chikwadraat.socialfridge.measuring.Unit;
 import be.kuleuven.cs.chikwadraat.socialfridge.model.FridgeItem;
 import be.kuleuven.cs.chikwadraat.socialfridge.model.Measure;
 import be.kuleuven.cs.chikwadraat.socialfridge.util.AdapterUtils;
 
 /**
- * Fragment displaying the ingredients in a fridge.
+ * Fragment displaying a list of ingredients.
  * Use the {@link IngredientsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class IngredientsFragment extends Fragment {
+public class IngredientsFragment extends ListFragment implements SearchView.OnQueryTextListener, SearchView.OnCloseListener,
+        MeasureDialog.OnMeasureSetListener {
 
     private static final String TAG = "IngredientsFragment";
 
-    private static final int LOADER_INGREDIENTS = 1;
+    private static final String STATE_QUERY = "ingredients_search_query";
+    private static final String STATE_ITEMS = "ingredients_items";
+    private static final String STATE_EDITING_ITEM = "ingredients_editing_item";
 
-    private ListView ingredientsList;
+    private ArrayList<FridgeItem> ingredients = new ArrayList<FridgeItem>();
     private IngredientsListAdapter ingredientsAdapter;
+    private int editingItemPosition;
+
+    private SearchView searchView;
+    private String searchQuery;
+
+    private IngredientsListener listener;
 
     /**
      * Create a new candidates fragment.
@@ -51,44 +68,165 @@ public class IngredientsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_fridge_ingredients, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        ingredientsList = (ListView) view.findViewById(R.id.fridge_ingredient_list);
+        if (savedInstanceState != null) {
+            searchQuery = savedInstanceState.getString(STATE_QUERY);
+            ingredients = savedInstanceState.getParcelableArrayList(STATE_ITEMS);
+            editingItemPosition = savedInstanceState.getInt(STATE_EDITING_ITEM, -1);
+        }
 
-        return view;
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.simple_card_list, container);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        ingredientsAdapter = new IngredientsListAdapter(getActivity());
+        ingredientsAdapter = new IngredientsListAdapter(getActivity(), ingredients);
+        setListAdapter(ingredientsAdapter);
+    }
 
-        // TODO Remove dummy items
-        List<FridgeItem> items = new ArrayList<FridgeItem>();
-        items.add(new FridgeItem("Eggs", R.drawable.eggs, new Measure(6, Unit.PIECES)));
-        AdapterUtils.setAll(ingredientsAdapter, items);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
 
-        ingredientsList.setAdapter(ingredientsAdapter);
+        inflater.inflate(R.menu.fridge_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint(getString(R.string.fridge_search_hint));
+        searchView.setOnQueryTextListener(this);
+        searchView.setOnCloseListener(this);
+
+        if (!TextUtils.isEmpty(searchQuery)) {
+            String query = searchQuery;
+            MenuItemCompat.expandActionView(searchItem);
+            searchIngredients(query);
+        }
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        searchIngredients(newText);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        // When the search is "committed" by the user, then hide the keyboard so the user can
+        // more easily browse the list of results.
+        if (searchView != null) {
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+            }
+            searchView.clearFocus();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onClose() {
+        searchIngredients(null);
+        return false;
+    }
+
+    protected void searchIngredients(String query) {
+        searchQuery = query;
+        if (searchView != null && !TextUtils.equals(searchView.getQuery(), query)) {
+            searchView.setQuery(searchQuery, false);
+        }
+        ingredientsAdapter.getFilter().filter(query);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        FridgeItem item = (FridgeItem) l.getItemAtPosition(position);
+        editingItemPosition = position;
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment current = getFragmentManager().findFragmentByTag("dialog");
+        MeasureDialogFragment fragment;
+        if (current != null) {
+            fragment = (MeasureDialogFragment) current;
+        } else {
+            fragment = MeasureDialogFragment.newInstance();
+        }
+        fragment.setTitle(item.getName());
+        fragment.setMeasure(item.getQuantity());
+        fragment.setTargetFragment(this, 0);
+        fragment.show(ft, "dialog");
+    }
+
+    @Override
+    public void onMeasureSet(Measure measure) {
+        if (editingItemPosition >= 0) {
+            FridgeItem item = (FridgeItem) getListView().getItemAtPosition(editingItemPosition);
+            item.setQuantity(measure);
+            if (listener != null) {
+                listener.onFridgeItemUpdated(item);
+            }
+            ingredientsAdapter.notifyDataSetChanged();
+        }
+        editingItemPosition = -1;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_QUERY, searchView.getQuery().toString());
+        outState.putParcelableArrayList(STATE_ITEMS, ingredients);
+        outState.putInt(STATE_EDITING_ITEM, editingItemPosition);
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        try {
+            listener = (IngredientsListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement IngredientsListener");
+        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+        listener = null;
+    }
+
+    public List<FridgeItem> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<FridgeItem> ingredients) {
+        if (ingredientsAdapter != null) {
+            AdapterUtils.setAll(ingredientsAdapter, ingredients);
+        } else {
+            this.ingredients = new ArrayList<FridgeItem>(ingredients);
+        }
+    }
+
+    public interface IngredientsListener {
+
+        public void onFridgeItemUpdated(FridgeItem item);
+
+        public void onFridgeItemRemoved(FridgeItem item);
+
     }
 
     public class IngredientsListAdapter extends ArrayAdapter<FridgeItem> {
 
-        public IngredientsListAdapter(Context context) {
-            super(context, R.layout.fridge_list_item);
+        public IngredientsListAdapter(Context context, List<FridgeItem> items) {
+            super(context, R.layout.fridge_list_item, items);
         }
 
         @Override
